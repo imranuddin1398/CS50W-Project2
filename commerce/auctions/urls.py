746a35217b5fd4e6a88a9210c7168b8ac5435e5b
imranuddin1398 <imranuddin1398@gmail.com>
2024-09-21 17:37:20 +0500

    Add files via upload

---
+++ commerce/auctions/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("category", views.category, name="category"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("clisting", views.clisting, name="clisting"),
+    path("listings/<int:id>", views.listings, name="listings"),
+    path("removeWatchlist/<int:id>", views.removeWatchlist, name="removeWatchlist"),
+    path("addWatchlist/<int:id>", views.addWatchlist, name="addWatchlist"),
+    path("addComment/<int:id>", views.addComment, name="addComment"),
+    path("addBid/<int:id>", views.addBid, name="addBid"),
+    path("closeAuction/<int:id>", views.closeAuction, name="closeAuction"),
+]
